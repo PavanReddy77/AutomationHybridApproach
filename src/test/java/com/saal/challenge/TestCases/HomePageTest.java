fb3ff76d99a1c0884a2ae675453f1b00fa324d86
PavanReddy77 <Pavankrishnan1993@Gmail.com>
2020-04-16 15:23:25 +0530

    Added Code for HomePageTitle

--- src/test/java/com/saal/challenge/TestCases/HomePageTest.java
+++ src/test/java/com/saal/challenge/TestCases/HomePageTest.java
@@ -1,6 +1,7 @@
 package com.saal.challenge.TestCases;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
 
 import com.saal.challenge.BaseClass.TestBase;
 
@@ -11,5 +12,11 @@ public class HomePageTest extends TestBase
 	public HomePageTest(WebDriver driver)
 	{
 		this.driver = driver;
+	}
+	
+	@Test
+	public void printHomePageUserName()
+	{
+		System.out.println("Home Page User Name is Pavan");
 	}
 }
